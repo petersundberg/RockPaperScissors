275e17f4419721a6c444aeef4f376ad3355bc37d
crazvik <crazvik@gmail.com>
2020-10-06 12:34:52 +0200

    Added comments

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 
 let i, lastValue, myPreviousHandText, cpuPreviousHandText;
 let isChecked, finalScore, wonSet;
+let buttonPress, myScore, cpuScore;
+let sets, gameOver;
 
 export default class App extends React.Component {
   constructor(props) {
@@ -15,14 +17,16 @@ export default class App extends React.Component {
     i = 0;
     myPreviousHandText = ""
     cpuPreviousHandText = ""
+    buttonPress = false;
+    myScore = 0;
+    cpuScore = 0;
+    sets = 0;
+    gameOver = 0;
   }
-  buttonPress = false;
-  myScore = 0;
-  cpuScore = 0;
-  sets = 0;
-  gameOver = 0;
-  winLose = '';
 
+  /**
+   * Hanterar värdesändringar på sidan, från radioknapparna och submitknappen.
+   */
   handleChange(event) {
     this.setState({value: event.target.value});
     lastValue = event.target.value;
@@ -30,26 +34,32 @@ export default class App extends React.Component {
     event.target.defaultChecked = false;
   } 
 
+  /**
+   * Hanterar knapptryckningen när man väljer set.
+   */
   handleSubmit(event) {
     event.preventDefault();
     if(isChecked) {
-      if(this.buttonPress===false) {
-        this.sets = parseInt(lastValue);
-        this.buttonPress = true;
+      if(buttonPress===false) {
+        sets = parseInt(lastValue);
+        buttonPress = true;
       }
       this.uncheckRadiobuttons();
       this.handleChange(event)
     }
   }
 
+  /**
+   * Hanterar knapptryckningen när man väljer hand.
+   */
   handleGame(event) {
     event.preventDefault();
     if(isChecked) {
-      if(i<this.sets) {
+      if(i<sets) {
         this.checkSetWinner(lastValue, Choice[this.generateCpuHand()])
         i++;
       }
-      if(i===this.sets || this.gameOver===0) {
+      if(i===sets || gameOver===0) {
         this.checkGameWinner();
       }
       this.uncheckRadiobuttons();
@@ -57,20 +67,29 @@ export default class App extends React.Component {
     }
   }
 
+  /**
+   * Avmarkerar radioknappen som är markerad
+   */
   uncheckRadiobuttons() {
-    var radioBtns = document.getElementsByClassName("choiceButton");
-    for(var i=0; i<radioBtns.length; i++) {
+    let radioBtns = document.getElementsByClassName("choiceButton");
+    for(let i=0; i<radioBtns.length; i++) {
       if(radioBtns.item(i).checked) {
         radioBtns.item(i).checked = false;
       }
     }
   }
 
+  /**
+   * Genererar en hand åt datorn. Börjar bara med att slumpa fram ett tal mellan
+   * 0-2. När första rundan är avklarad börjar datorn kolla om den vann eller inte. 
+   * Om den vann lägger den sin förra hand och en slumpad i en array, där den
+   * nya handen slumpas fram. (För att behålla lite slumpmässighet). Förlorar
+   * datorn förra handen slumpar den fram båda värden istället för att använda
+   * den förra handen.
+   */
   generateCpuHand() {
     let cpuChoice = new Array(2);
-    if(i===0) {
-      return Math.floor(Math.random() * Math.floor(3))
-    } else if(wonSet===1) {
+    if(wonSet===1) {
         cpuChoice[0] = Choice[cpuPreviousHandText];
         cpuChoice[1] = Math.floor(Math.random() * Math.floor(3))
         return cpuChoice[Math.floor(Math.random() * Math.floor(2))]
@@ -83,6 +102,12 @@ export default class App extends React.Component {
     }
   }
 
+  /**
+   * Kollar vem som vann det senaste settet och lägger till poäng till vinnaren.
+   * Blir settet oavgjort spelas rundan om.
+   * @param {Spelarens senaste hand} myHand 
+   * @param {Datorns senaste hand} cpuHand 
+   */
   checkSetWinner(myHand, cpuHand) {
     myPreviousHandText = myHand;
     cpuPreviousHandText = cpuHand;
@@ -92,31 +117,31 @@ export default class App extends React.Component {
           i--;
           wonSet = 3;
         } else if(cpuHand==="Paper") {
-          this.cpuScore++;
+          cpuScore++;
           wonSet = 1;
         } else {
-          this.myScore++;
+          myScore++;
           wonSet = 2;
         }
         break;
       case "Paper":
         if(cpuHand==="Rock") {
-          this.myScore++;
+          myScore++;
           wonSet = 2;
         } else if(cpuHand==="Paper") {
           i--;
           wonSet = 3;
         } else {
-          this.cpuScore++;
+          cpuScore++;
           wonSet = 1;
         }
         break;
       default:
         if(cpuHand==="Rock") {
-          this.cpuScore++;
+          cpuScore++;
           wonSet = 1;
         } else if(cpuHand==="Paper") {
-          this.myScore++;
+          myScore++;
           wonSet = 2;
         } else {
           i--;
@@ -126,39 +151,46 @@ export default class App extends React.Component {
     }
   }
 
+  /**
+   * Kontrollerar vem som vunnit spelet och skickar poängen och vinstmeddelandet
+   * till en komponent.
+   */
   checkGameWinner() {
-    if(this.myScore===this.sets) {
-      this.gameOver = 1;
-      finalScore = <FinalScore winLose="You win" myScore={this.myScore} cpuScore={this.cpuScore}/>
-    } else if(this.cpuScore===this.sets) {
-        this.gameOver = 1;
-        finalScore = <FinalScore winLose="Cpu wins" myScore={this.myScore} cpuScore={this.cpuScore}/>
-    } else if(this.myScore+this.sets%2===this.sets && this.sets>1) {
-        this.gameOver = 1;
-        finalScore = <FinalScore winLose="You win" myScore={this.myScore} cpuScore={this.cpuScore}/>
-    } else if(this.cpuScore+this.sets%2===this.sets && this.sets>1) {
-        this.gameOver = 1;
-        finalScore = <FinalScore winLose="Cpu wins" myScore={this.myScore} cpuScore={this.cpuScore}/>
-    } else if(this.myScore+this.sets%2+1===this.sets && this.sets>3) {
-        this.gameOver = 1;
-        finalScore = <FinalScore winLose="You win" myScore={this.myScore} cpuScore={this.cpuScore}/>
-    } else if(this.cpuScore+this.sets%2+1===this.sets && this.sets>3) {
-        this.gameOver = 1;
-        finalScore = <FinalScore winLose="Cpu wins" myScore={this.myScore} cpuScore={this.cpuScore}/>
+    if(myScore===sets) {
+      gameOver = 1;
+      finalScore = <FinalScore winLose="You win" myScore={myScore} cpuScore={cpuScore}/>
+    } else if(cpuScore===sets) {
+        gameOver = 1;
+        finalScore = <FinalScore winLose="Cpu wins" myScore={myScore} cpuScore={cpuScore}/>
+    } else if(myScore+sets%2===sets && sets>1) {
+        gameOver = 1;
+        finalScore = <FinalScore winLose="You win" myScore={myScore} cpuScore={cpuScore}/>
+    } else if(cpuScore+sets%2===sets && sets>1) {
+        gameOver = 1;
+        finalScore = <FinalScore winLose="Cpu wins" myScore={myScore} cpuScore={cpuScore}/>
+    } else if(myScore+sets%2+1===sets && sets>3) {
+        gameOver = 1;
+        finalScore = <FinalScore winLose="You win" myScore={myScore} cpuScore={cpuScore}/>
+    } else if(cpuScore+sets%2+1===sets && sets>3) {
+        gameOver = 1;
+        finalScore = <FinalScore winLose="Cpu wins" myScore={myScore} cpuScore={cpuScore}/>
     }
   }
 
+  /**
+   * Renderar sidan med hjälp av en if-sats ifall man valt set eller inte och om
+   * spelet är slut eller inte.
+   */
   render() {
     let header;
     let scoreBoard;
-
-    if(!this.buttonPress) {
+    if(!buttonPress) {
       header = <SetText header="Välj antal set"/>
     } else {
-      header = <SetText header="Välj hand" rounds={this.sets + " set"}/>
-      scoreBoard = <UpdateScoreboard myScore={this.myScore} cpuScore={this.cpuScore} />
+      header = <SetText header="Välj hand" rounds={sets + " set"}/>
+      scoreBoard = <UpdateScoreboard myScore={myScore} cpuScore={cpuScore} />
     }
-    if(!this.buttonPress && this.gameOver===0) {
+    if(!buttonPress && gameOver===0) {
       return (
         <div className="App">
         {header}
@@ -196,7 +228,7 @@ export default class App extends React.Component {
         </form>
       </div>
       );
-    } else if(this.buttonPress && this.gameOver===0) {
+    } else if(buttonPress && gameOver===0) {
       return(
         <div className="App">
         {header}
@@ -248,11 +280,17 @@ export default class App extends React.Component {
   }
 }
 
+/**
+ * Ändrar instruktionerna på sidan.
+ */
 function SetText(props) {
   return <div><p>{props.rounds}</p>
         <p>{props.header}</p></div>
 }
 
+/**
+ * Uppdaterar poängräkningen och båda spelarnas senaste hand.
+ */
 function UpdateScoreboard(props) {
   return <div>
     <p>{"Your score: " + props.myScore}</p>
@@ -262,6 +300,9 @@ function UpdateScoreboard(props) {
   </div>
 }
 
+/**
+ * Skriver ut vinstmeddelande och båda spelarnas poäng.
+ */
 function FinalScore(props) {
   return <div>
     <h1>{props.winLose} <br/>
